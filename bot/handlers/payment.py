7b71dbed6e7073de93575f166e22fec803ecc6a5
Ale007XD <alex.deloverov@gmail.com>
2026-03-10 08:31:49 +0700

    Update payment.py

--- bot/handlers/payment.py
+++ bot/handlers/payment.py
@@ -1,19 +1,14 @@
-from aiogram.filters import Command
 from aiogram import Router
-from aiogram.types import Message, PreCheckoutQuery
+from aiogram.types import Message, PreCheckoutQuery, SuccessfulPayment
+from bot.handlers.tarot import process_reading  # импортируем
 
 router = Router()
 
-
 @router.pre_checkout_query()
-async def pre_checkout(pre_checkout_query: PreCheckoutQuery):
+async def pre_checkout(pre: PreCheckoutQuery):
+    await pre.answer(ok=True)
 
-    await pre_checkout_query.answer(ok=True)
-
-
-@router.message()
-async def success(message: Message):
-
-    await message.answer(
-        "Payment successful. Your destiny is revealed 🔮"
-    )
+@router.message(lambda m: m.successful_payment)
+async def success_payment(message: Message):
+    await message.answer("✅ Оплата прошла! Готовы узнать судьбу?")
+    await process_reading(message, paid=1)  # paid=1
