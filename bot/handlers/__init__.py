696bff11906be3ccb016ad015e81b182453698cb
Ale007XD <alex.deloverov@gmail.com>
2026-06-09 08:33:24 +0700

    Add history module to handlers __all__ list

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -4,5 +4,6 @@ from . import start
 from . import tarot
 from . import payment
 from . import referral
+from . import history
 
-__all__ = ['start', 'tarot', 'payment', 'referral']
+__all__ = ['start', 'tarot', 'payment', 'referral', 'history']
